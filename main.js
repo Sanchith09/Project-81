f1a707a13f0fc1c68c173769c3a537ce5002a061
Sanchith09 <101087275+Sanchith09@users.noreply.github.com>
2022-08-23 17:33:25 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+canvas =
+document.getElementById("olympiccanvas");
+ctx= canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.linewidth = 1;
+ctx.rect(150, 143, 430, 200);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "blue"
+ctx.linewidth = 5;
+ctx.arc(250, 210, 40,0, 2* Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "black"
+ctx.linewidth = 5;
+ctx.arc(300, 210, 40,0, 2* Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "red"
+ctx.linewidth = 5;
+ctx.arc(350, 210, 40,0, 2* Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "yellow"
+ctx.linewidth = 5;
+ctx.arc(275, 250, 40,0, 2* Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "green"
+ctx.linewidth = 5;
+ctx.arc(315, 250, 40,0, 2* Math.PI);
+ctx.stroke();
+
+
+
